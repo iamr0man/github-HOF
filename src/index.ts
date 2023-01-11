d23c67b40b6e8c75ca26f9f281347b07d8812c38
iamr0man <koriny4@gmail.com>
2023-01-11 12:14:01 +0200

    feat(request): configure request for getting repositories owners; add types;

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,42 @@
 import * as dotenv from 'dotenv';
 import * as https from 'https';
 import { logError, logSuccess } from './logger';
+import { IncomingMessage } from 'http';
+import { Owner, Repository, Response } from './result.types';
 
 dotenv.config();
 
 const GITHUB_API_URL = 'https://api.github.com';
-const MAX_REPO_PER_PAGE = 100;
+const MAX_REPO_PER_PAGE = 3;
+const USER_AGENT =
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36';
 
 enum SORT {
   stars = 'stars',
 }
 
-export async function getRepositories(
-  language: string | undefined,
-): Promise<[] | Error> {
+const getRepositoriesRequest = (
+  language: string,
+): Promise<Response> | never => {
+  const queryCreated = 'created:">2018-01-30';
   const searchRepoUrl = `${GITHUB_API_URL}/search/repositories`;
-  // const urlWithQuery = `${searchRepoUrl}?l${language}&sort=${SORT.stars}&per_page=${MAX_REPO_PER_PAGE}`;
+  const urlWithQuery = `${searchRepoUrl}?q=${queryCreated}&l=${language}&sort=${SORT.stars}&per_page=${MAX_REPO_PER_PAGE}`;
   const options = {
     headers: {
+      'user-agent': USER_AGENT,
       Accept: 'application/vnd.github+json',
-      Authorization: 'Bearer ' + process.env.GITHUB_TOKEN,
       'X-GitHub-Api-Version': '2022-11-28',
     },
-    hostname: 'api.github.com',
-    path: `/search/repositories?l${language}&sort=${SORT.stars}&per_page=${MAX_REPO_PER_PAGE}`,
-    method: 'GET',
   };
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  options.agent = new https.Agent(options);
+  return new Promise((resolve) => {
+    const callback = (res: IncomingMessage) => {
+      if (res.statusCode === 422) {
+        throw new Error('Validation failed, or the endpoint has been spammed.');
+      }
+      if (res.statusCode === 503) {
+        throw new Error('Service unavailable');
+      }
 
-  const req = https
-    .request(options, (res) => {
       let data = '';
 
       res.on('data', (chunk) => {
@@ -40,19 +45,81 @@ export async function getRepositories(
 
       res.on('end', () => {
         logSuccess(data);
-        return JSON.parse(data);
+        resolve(JSON.parse(data));
       });
-    })
-    .on('error', (err) => {
-      logError(err);
-    })
-    .end();
-  // eslint-disable-next-line no-console
-  console.log(req);
+    };
 
-  return Promise.resolve([]);
+    https.request(urlWithQuery, options, callback).end();
+  });
+};
+const getOwnerRequest = (userName: string): Promise<Owner> | never => {
+  const searchUserUrl = `${GITHUB_API_URL}/users`;
+  const urlWithQuery = `${searchUserUrl}/${userName}`;
+  const options = {
+    headers: {
+      'user-agent': USER_AGENT,
+      Accept: 'application/vnd.github+json',
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  };
+  return new Promise((resolve) => {
+    const callback = (res: IncomingMessage) => {
+      if (res.statusCode === 422) {
+        throw new Error('Validation failed, or the endpoint has been spammed.');
+      }
+      if (res.statusCode === 503) {
+        throw new Error('Service unavailable');
+      }
+
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        logSuccess(data);
+        resolve(JSON.parse(data));
+      });
+    };
+
+    https.request(urlWithQuery, options, callback).end();
+  });
+};
+
+const getOwnerDetails = async (repository: Repository) => {
+  return getOwnerRequest(repository.owner.login);
+};
+
+type Result = [Repository, Owner][];
+
+export async function getRepositories(
+  language: string | undefined,
+): Promise<Result | Error> {
+  if (!language) {
+    return Promise.reject(new Error('Wrong language input type.'));
+  }
+  try {
+    const response: Response = await getRepositoriesRequest(language);
+    const listOfRepositories = response.items;
+
+    const ownerPromises: Promise<Owner>[] = listOfRepositories.reduce(
+      (acc, repository) => [...acc, getOwnerDetails(repository)],
+      [] as Promise<Owner>[],
+    );
+    const owners = await Promise.all(ownerPromises);
+
+    const result: Result = listOfRepositories.reduce(
+      (acc, curr, index) => [...acc, [curr, owners[index]]],
+      [] as Result,
+    );
+    return Promise.resolve(result);
+  } catch (err) {
+    logError(err);
+    return Promise.reject(err);
+  }
 }
 
-getRepositories('javascript')
-  .then((res) => console.log(res))
-  .catch((err) => console.log(err));
+// getRepositories('javascript')
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
