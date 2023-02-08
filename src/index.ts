491a890f1710be4196dd98e36c195195a8502cae
iamr0man <koriny4@gmail.com>
2023-02-08 20:04:24 +0200

    feat(index.ts): clear all unnecessary from main index;

--- src/index.ts
+++ src/index.ts
@@ -1,171 +1,20 @@
 import * as dotenv from 'dotenv';
-
-import { logError } from './logger';
-
-import { WrongInputError } from './errors/wrongInputError';
-import { EmptyInputError } from './errors/emptyInputError';
-import { MaxLengthError } from './errors/maxLengthError';
-
-import { Owner, RepositoryResponse } from './result.types';
-import { request } from './request';
-
-import { createQueue } from './queue/newConcurrentQueue';
-import {
-  RateLimit,
-  RateLimitResponse,
-  Result,
-  Task,
-  TaskResult,
-} from './queue/concurrentQueue.types';
-
 dotenv.config();
 
-const { GITHUB_API_URL } = process.env;
+import { logError, logSuccess } from './logger';
 
-const MAX_REPO_PER_PAGE = 5;
-const MAX_LANGUAGE_LENGTH = 100;
-
-enum SORT {
-  stars = 'stars',
-}
-
-const getRepositoriesRequest = (
-  language: string,
-): Promise<RepositoryResponse> | never => {
-  const queryCreated = 'created:">2001-01-01';
-  const searchRepoUrl = `${GITHUB_API_URL}/search/repositories`;
-  const urlWithQuery = `${searchRepoUrl}?q=${queryCreated}&l=${language}&sort=${SORT.stars}&per_page=${MAX_REPO_PER_PAGE}`;
-
-  return request<RepositoryResponse>(urlWithQuery);
-};
-const getOwnerRequest = (userName: string): Promise<Owner> | never => {
-  const searchUserUrl = `${GITHUB_API_URL}/users`;
-  const urlWithQuery = `${searchUserUrl}/${userName}`;
-
-  return request<Owner>(urlWithQuery);
-};
-
-const getRateLimitRequest = (): Promise<RateLimitResponse> | never => {
-  const rateLimitUrl = `${GITHUB_API_URL}/rate_limit`;
-
-  return request<RateLimitResponse>(rateLimitUrl);
-};
-
-const generateId = () => Math.floor(Math.random() * 100);
-
-const GET_REPOSITORIES = 'GET_REPOSITORIES';
-const GET_OWNER = 'GET_OWNER';
-const GET_RATE_LIMIT = 'GET_RATE_LIMIT';
+import { validate } from './validation';
+import { handleRepositoriesByQueue } from './queue/handleRequests';
+import { Result } from './queue/concurrentQueue.types';
 
 export async function getRepositories(
   language: string | undefined,
-): Promise<Result | Error> {
-  if (typeof language !== 'string') {
-    const error = new WrongInputError('Wrong input data');
-    logError(error);
-    return Promise.reject(error);
-  }
-  if (language.length === 0) {
-    const error = new EmptyInputError('Empty input');
-    logError(error);
-    return Promise.reject(error);
-  }
-  if (language.length > MAX_LANGUAGE_LENGTH) {
-    const error = new MaxLengthError('Max length overcome');
-    logError(error);
-    return Promise.reject(error);
-  }
+): Promise<Result> {
+  validate(language);
 
-  try {
-    const initialState: Task[] = [];
-    const taskResult: TaskResult = [];
-
-    const taskCallbackMap: Record<string, (props?: any) => Promise<unknown>> = {
-      [GET_REPOSITORIES]: (language) => getRepositoriesRequest(language),
-      [GET_OWNER]: (username) => getOwnerRequest(username),
-      [GET_RATE_LIMIT]: getRateLimitRequest,
-    };
-
-    const process = (task: Task) => {
-      console.log('Handle task', task);
-      if (!task.name) {
-        return Promise.reject('');
-      }
-
-      const fn = taskCallbackMap[task.name];
-
-      let repository = null;
-      if (taskResult.length) {
-        const [repositories] = taskResult;
-        repository = (repositories as RepositoryResponse).items.pop();
-      }
-
-      switch (task.name) {
-        case GET_REPOSITORIES:
-          return fn(language);
-        case GET_OWNER:
-          return fn(repository?.owner.login);
-        default:
-          return fn();
-      }
-    };
-
-    const rateLimit = await getRateLimitRequest();
-    let searchRate: Pick<RateLimit, 'limit' | 'remaining'> =
-      rateLimit.resources.search;
-
-    const onFailed = (task: Task, err: unknown) => {
-      console.log('Failed', task.key, err);
-    };
-
-    initialState.push({
-      key: generateId(),
-      name: GET_REPOSITORIES,
-    });
-
-    const queue = createQueue(initialState, {
-      process,
-      onSucceed,
-      onFailed,
-    });
-    const result = [];
-    // eslint-disable-next-line no-inner-declarations,@typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    // eslint-disable-next-line no-inner-declarations
-    function onSucceed(task: Task, value: unknown) {
-      console.log('Complete', task, value);
-
-      switch (task.name) {
-        case GET_REPOSITORIES:
-          result.push(value);
-          taskResult.push(value);
-          (value as RepositoryResponse).items.forEach(() => {
-            queue.add({
-              key: generateId(),
-              name: GET_OWNER,
-            });
-          });
-          break;
-        case GET_OWNER:
-          result.push();
-          break;
-        case GET_RATE_LIMIT:
-          searchRate = (value as RateLimitResponse).resources.search;
-          break;
-        default:
-          break;
-      }
-    }
-
-    queue.start();
-
-    return Promise.resolve([]);
-  } catch (err) {
-    logError(err);
-    return Promise.reject(err);
-  }
+  return handleRepositoriesByQueue(language as string);
 }
 
 getRepositories('javascript')
-  .then((res) => console.log(res))
-  .catch((err) => console.log(err));
+  .then((res) => logSuccess(res))
+  .catch((err) => logError(err));
