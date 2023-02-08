d722cb7cb13985202cb63185d8bb6a05c22cd4df
iamr0man <koriny4@gmail.com>
2023-02-08 20:03:44 +0200

    feat(concurrentQueue.types.ts): add TaskResult types;

--- src/queue/concurrentQueue.types.ts
+++ src/queue/concurrentQueue.types.ts
@@ -1,57 +1,48 @@
-import { Owner, Repository } from '../result.types';
+import {
+  Owner,
+  RateLimitResponse,
+  Repository,
+  RepositoryResponse,
+} from '../result.types';
 
-export interface TaskItem {
-  name: string;
-  fn: () => Promise<any>;
-  result: any;
+export enum TaskName {
+  GET_REPOSITORIES = 'GET_REPOSITORIES',
+  GET_OWNER = 'GET_OWNER',
+  GET_RATE_LIMIT = 'GET_RATE_LIMIT',
 }
-export interface Params {
-  err: Error | string | null;
-  result: TaskItem;
-}
-export type ProcessHandlerCallback = (params: Params) => void;
-export type ProcessCallback<T> = (task: TaskItem) => Promise<T>;
-export type FailureCallback = (err: Error | string) => void;
-export type DoneCallback = (params: Params) => void;
-export type SuccessCallback = (result: TaskItem) => void;
-export type DrainCallback = () => void;
+
+export type TaskRepository = {
+  name: TaskName.GET_REPOSITORIES;
+  result: RepositoryResponse;
+};
+
+export type TaskOwner = {
+  name: TaskName.GET_OWNER;
+  result: Owner;
+};
+
+export type TaskRateLimit = {
+  name: TaskName.GET_RATE_LIMIT;
+  result: RateLimitResponse;
+};
+
+export type TaskResult = TaskOwner | TaskRepository | TaskRateLimit;
 
 export interface Task {
-  readonly key: number;
-  readonly name: string;
+  readonly name: TaskName;
 }
 
-export interface Queue {
-  readonly start: () => void;
-  readonly add: (task: Task) => void;
+export interface Queue<T> {
+  readonly start: () => Promise<void[]>;
+  readonly add: (task: T) => void;
 }
 
-export interface QueueFactoryOptions {
+export interface QueueFactoryOptions<T, R> {
   readonly concurrency?: number;
 
-  readonly process: (task: Task) => Promise<unknown>;
-  readonly onSucceed?: (task: Task, result: unknown) => void;
-  readonly onFailed?: (task: Task, err: unknown) => void;
+  readonly process: (task: T) => Promise<R>;
+  readonly onSucceed?: (task: T, result: R) => Promise<void>;
+  readonly onFailed?: (task: T, err: unknown) => Promise<void>;
 }
 
-export type TaskResult = unknown[];
-
-export type Result = [Repository, Owner][];
-
-export interface RateLimit {
-  limit: number;
-  remaining: number;
-  reset: number;
-  used: number;
-}
-
-export interface RateLimitResponse {
-  resources: {
-    core: RateLimit;
-    search: RateLimit;
-    graphql: RateLimit;
-    integration_manifest: RateLimit;
-    code_scanning_upload: RateLimit;
-  };
-  rate: RateLimit;
-}
+export type Result = Array<[Repository, Owner]>;
