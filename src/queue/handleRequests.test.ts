440babc0576c1fba564a8befe1540fd2ed896a60
iamr0man <koriny4@gmail.com>
2023-02-25 09:59:17 +0200

    test(handleRequests.test.ts): add mock requests; remove useless jest functions;

--- src/queue/handleRequests.test.ts
+++ src/queue/handleRequests.test.ts
@@ -1,64 +1,92 @@
-import { TaskNameError } from './concurrentQueue.types';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { expect, jest, describe, beforeEach, test, afterAll } from '@jest/globals';
 import { handleRepositoriesByQueue } from './handleRequests';
-import { expect, jest, describe, it } from '@jest/globals';
-import { RequestData } from '../request';
-import { Owner, RepositoryResponse } from '../result.types';
 
-type RequestHeaders = Promise<Pick<RequestData<RepositoryResponse>, 'headers'>>;
-type OwnerRequest = Promise<Omit<RequestData<Pick<Owner, 'login'>>, 'headers'>>;
+jest.setTimeout(10000);
 
 describe('handleRepositoriesByQueue', () => {
-  it('should handle empty repository list', async () => {
+  const OLD_ENV = process.env;
+  let mock: MockAdapter | null = null;
+
+  const setRateLimitError = (url: string | RegExp) => {
+    mock?.onGet(url).reply(
+      403,
+      {
+        message: 'API rate limit exceeded',
+      },
+      { 'x-ratelimit-remaining': '0' },
+    );
+  };
+
+  const setValidationError = (url: string | RegExp) => {
+    mock?.onGet(url).reply(404, {
+      message: 'Request failed with status code 404',
+    });
+  };
+
+  beforeEach(() => {
+    mock = new MockAdapter(axios);
+    jest.resetModules();
+    process.env = { ...OLD_ENV };
+  });
+
+  afterAll(() => {
+    mock?.restore();
+    process.env = OLD_ENV;
+  });
+
+  test('should handle empty repository list', async () => {
     const result = await handleRepositoriesByQueue('javascript', 0);
     expect(result).toEqual([]);
   });
 
-  it('should handle single repository', async () => {
+  test('should handle single repository', async () => {
     const result = await handleRepositoriesByQueue('javascript', 1);
     expect(result.length).toEqual(1);
     expect(result[0][1]).not.toBeNull();
   });
 
-  it('should handle multiple repositories', async () => {
+  test('should handle repository rate limit error', async () => {
+    const searchRepoUrl = /\/search\/repositories\/w+/g;
+
+    setRateLimitError(searchRepoUrl);
+
+    const result = await handleRepositoriesByQueue('javascript', 1);
+    expect(result).toEqual([]);
+  });
+
+  test('should handle owner rate limit error', async () => {
+    const searchUserUrl = /\/users\/\w+/g;
+
+    setRateLimitError(searchUserUrl);
+
+    const result = await handleRepositoriesByQueue('java', 1);
+    expect(result).toEqual([expect.any(Object), null]);
+  });
+
+  test('should handle multiple repositories', async () => {
     const result = await handleRepositoriesByQueue('javascript', 10);
     expect(result.length).toEqual(10);
     expect(result.every(([_, owner]) => owner !== null)).toBe(true);
   });
 
-  it('should handle rate limit error', async () => {
-    const getRepositoriesRequest = jest.fn<() => RequestHeaders>().mockResolvedValueOnce({
-      headers: { 'x-ratelimit-remaining': '0' },
+  test('should handle error when getting repositories', async () => {
+    const searchRepoUrl = /\/search\/repositories\/w+/g;
+
+    mock?.onGet(searchRepoUrl).reply(404, {
+      message: 'Request failed with status code 404',
     });
+
     const result = await handleRepositoriesByQueue('javascript', 1);
-    expect(getRepositoriesRequest).toHaveBeenCalled();
-    expect(result).toEqual([[null, TaskNameError.RATE_LIMIT_ERROR]]);
+    expect(result).toEqual([]);
   });
 
-  it('should handle empty owner login', async () => {
-    const getOwnerRequest = jest.fn<() => OwnerRequest>().mockResolvedValueOnce({
-      data: { login: '' },
-    });
-    const result = await handleRepositoriesByQueue('javascript', 1);
-    expect(getOwnerRequest).toHaveBeenCalled();
-    expect(result).toEqual([[expect.any(Object), null]]);
-  });
+  test('should handle error when getting owner', async () => {
+    const searchUserUrl = /\/users\/\w+/g;
+    setValidationError(searchUserUrl);
 
-  it('should handle error when getting repositories', async () => {
-    const getRepositoriesRequest = jest.fn<() => Promise<string>>().mockRejectedValueOnce('error');
     const result = await handleRepositoriesByQueue('javascript', 1);
-    expect(getRepositoriesRequest).toHaveBeenCalled();
-    expect(result).toEqual([[null, null]]);
-  });
-
-  it('should handle error when getting owner', async () => {
-    const getOwnerRequest = jest.fn<() => Promise<string>>().mockRejectedValueOnce('error');
-    const result = await handleRepositoriesByQueue('javascript', 1);
-    expect(getOwnerRequest).toHaveBeenCalled();
-    expect(result).toEqual([[expect.any(Object), null]]);
-  });
-
-  it('should handle empty task', async () => {
-    const result = await handleRepositoriesByQueue('javascript', 1);
-    expect(result).not.toBeNull();
+    expect(result).toEqual([expect.any(Object), null]);
   });
 });
