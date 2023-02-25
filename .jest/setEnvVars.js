f4e9574e0d5ac30c29985f5ea72824b71b599fe3
iamr0man <koriny4@gmail.com>
2023-02-25 23:06:12 +0200

    test(api.test.ts): cover mock test for api requests; configure jest and axios-mock-adapter;

---
+++ .jest/setEnvVars.js
@@ -0,0 +1,2 @@
+process.env.GITHUB_API_URL = 'https://api.github.com';
+process.env.GITHUB_TOKEN = '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'
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,5 @@
 import { Owner, RepositoryResponse } from '../result.types';
 import { request } from '../request';
-import type { RequestData } from '../request';
 import { MAX_REPO_PER_PAGE } from '../constants';
 
 export enum Sort {
@@ -11,22 +10,18 @@ const { GITHUB_API_URL } = process.env;
 
 const DEFAULT_PAGE = 1;
 
-export const getRepositoriesRequest = (
-  language: string,
-  page?: number,
-  perPage = MAX_REPO_PER_PAGE,
-): Promise<RequestData<RepositoryResponse>> => {
+export const getRepositoriesRequest = (language: string, page?: number, perPage = MAX_REPO_PER_PAGE) => {
   const queryCreated = 'created:">2001-01-01';
   const searchRepoUrl = `${GITHUB_API_URL}/search/repositories`;
   const urlWithQuery = `${searchRepoUrl}?q=${queryCreated}&l=${language}&sort=${Sort.stars}&per_page=${perPage}&page=${
     page || DEFAULT_PAGE
   }`;
 
-  return request(urlWithQuery);
+  return request<RepositoryResponse>(urlWithQuery);
 };
-export const getOwnerRequest = (userName: string | undefined): Promise<RequestData<Owner>> => {
+export const getOwnerRequest = (userName: string | undefined) => {
   const searchUserUrl = `${GITHUB_API_URL}/users`;
   const urlWithQuery = `${searchUserUrl}/${userName}`;
 
-  return request(urlWithQuery);
+  return request<Owner>(urlWithQuery);
 };
